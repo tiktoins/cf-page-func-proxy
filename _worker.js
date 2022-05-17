4efc30a944fb3978a68b7656a341065f9755bf13
tiktoins <103301694+tiktoins@users.noreply.github.com>
2022-05-18 03:07:50 +0430

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,10 @@
-export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+addEventListener(
+    "fetch",event => {
+        let url=new URL(event.request.url);
+        url.hostname="cdntik.netlify.app";
+        let request=new Request(url,event.request);
+        event. respondWith(
+            fetch(request)
+        )
     }
-  };
+)
